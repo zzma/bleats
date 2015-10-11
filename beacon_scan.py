10b4592323424325e59406b9f28c920e424d7314
Zane Ma <amenaz1@gmail.com>
2015-10-11 12:47:55 -0700

    basic scan and sqlite lookup

--- beacon_scan.py
+++ beacon_scan.py
@@ -1,4 +1,5 @@
 from bluetooth.ble import BeaconService
+import sqlite3
 
 class Beacon(object):
 
@@ -17,11 +18,16 @@ class Beacon(object):
                         MINOR=self._minor, POWER=self._power, RSSI=self._rssi)
         return ret
 
-service = BeaconService()
+conn = sqlite3.connect('students.db')
+cursor = conn.cursor()
+
+service = BeaconService("hci1")
 devices = service.scan(2)
 
 for address, data in list(devices.items()):
     b = Beacon(data, address)
+    cursor.execute("SELECT * FROM students WHERE uuid = '" + b._uuid + "' LIMIT 1")
+    print(cursor.fetchone())
     print(b)
 
 print("Done.")
