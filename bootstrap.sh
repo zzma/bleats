5327353af03074ec387bf30f336280b3d48202f0
zanema <amenaz1@gmail.com>
2015-10-10 19:08:27 -0500

    bootstrap install script

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install python-pip
+sudo apt-get install python-dev
+sudo apt-get install libbluetooth-dev
+sudo apt-get install libglib2.0-dev
+sudo apt-get install libboost-all-dev
+sudo pip install pybluez
+sudo pip install gattlib
+
+
