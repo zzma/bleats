b2d05e35b26f46a1f512668be38434ba71573a71
Bob King <kingbob2015@hotmail.com>
2015-11-07 15:40:34 -0600

    code sending and AuthDB for code check

---
+++ sendCode.py
@@ -0,0 +1,27 @@
+import smtplib
+from random import randint
+import sqlite3
+
+
+uuid = '11111111-2222-3333-4444-555555555555'
+code = str(randint(1111111,9999999))
+
+db_filename = 'AuthDB.db'
+
+with sqlite3.connect(db_filename) as conn:
+	cursor = conn.cursor()
+	query = "update students set code='"+code+"' where uuid='"+uuid+"'"
+	cursor.execute(query) 
+
+
+message = """From: UIUC Bleats <uiucbleats@gmail.com>
+To: Student <5158678777@pm.sprint.com>
+Subject: Your Code
+
+Your code is  
+""" + code
+server = smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+server.login('uiucbleats','WirelessNetworks')
+server.sendmail('Your Professor','5158678777@pm.sprint.com',message)
+
